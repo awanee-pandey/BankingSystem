c66b2267d66538cfc384d1fc73e08f16a278b484
“Awanee <Awanee.pandey@gmail.com>
2021-01-21 20:15:17 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/* 1 */
+add(2,3);
+function add(a,b){
+    let sum = a+b;
+    console.log(sum)
+}
+
+/* 2 */
+// console.log(r);
+// let r = 'huda';
+
+/* 3 */
+console.log(y);
+var y = 'awanee';
+
+/* 4 */
+// a();
+b();
+
+const a= function(){
+
+}
+
+var b= function(){
+
+}
